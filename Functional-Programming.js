6372499eed51ea8a7a3f6abf9c2d42f4a256a554
Charles Crowley <CharlesCrowleyDev@gmail.com>
2022-12-28 22:13:53 +0100

    Update Functional-Programming.js

--- Functional-Programming.js
+++ Functional-Programming.js
@@ -1,1 +1,20 @@
+/*
 
+MAP
+
+The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+*/
+const array1 = [1, 4, 9, 16];
+const map1 = array1.map(x => x * 2);
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]
+
+/*
+
+FILTER
+
+REDUCE 
+
+
+
+*/
