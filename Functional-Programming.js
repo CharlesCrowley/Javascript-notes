d82bd7c4120915936d62d47b7d3bf322327c0535
Charles Crowley <CharlesCrowleyDev@gmail.com>
2023-01-01 19:04:57 +0100

    Update Functional-Programming.js

--- Functional-Programming.js
+++ Functional-Programming.js
@@ -172,6 +172,14 @@ console.log(`Gold winner: ${gold}, Silver Winner: ${silver}, Bronze Winner: ${br
 /*
 REDUCE 
 
+DESCRIPTION:
+You get an array of numbers, return the sum of all of the positives ones.
 
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
+Note: if there is nothing to sum, the sum is default to 0.
 */
+function positiveSum(arr) {
+   return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+}
+
