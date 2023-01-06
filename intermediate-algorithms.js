53d229a7e4767771f9fdd478c0f9d17840177cda
Charles Crowley <CharlesCrowleyDev@gmail.com>
2023-01-06 19:13:23 +0100

    Update intermediate-algorithms.js

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -55,3 +55,11 @@ function diffArray(arr1, arr2) {
 
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])); // returns 4. 
 
+/*
+Seek and Destroy
+You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+Note: You have to use the arguments object.
+*/
+
+
