83a4c79acf55d96354780ecffaea1d41534361ee
Charles <CharlesCrowleyDev@gmail.com>
2023-01-13 12:33:47 +0100

    Update intermediate-algorithms.js

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -161,3 +161,34 @@ return str.replace(/^([^aeiou]+)([a-z]+)/g, '$2$1ay');
 
 console.log(translatePigLatin("rhythm"));
 
+/*
+Search and Replace
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+*/
+
+function myReplace(str, before, after) {
+
+  let cap = /^[A-Z]/; // regex checks if the first character is a uppercase letter. 
+  
+  if (cap.test(before) === true) { // condition with a test to see if before argument is capital or not. 
+    return str.replace(before, after // replace the before word with afer word.
+    .replace(after[0], after[0].toUpperCase())) // the after arguments is also using the replace method because we want to capitalise it.
+  }
+  return str.replace(before, after.toLowerCase() ); // what happens if the first letter of before isn't capitalised. We use the toLowerCase() method 
+  // in the cases where the `after` argument is capitalised. 
+
+}
+
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")); //A quick brown fox leaped over the lazy dog
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting")) //He is Sitting on the couch
+console.log(myReplace("I think we should look up there", "up", "Down")) //I think we should look down there
+
+
