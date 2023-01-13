9a3f50b7d265d5f53398ca919f29e69c15e66b0c
Charles <CharlesCrowleyDev@gmail.com>
2023-01-13 11:00:34 +0100

    Update intermediate-algorithms.js

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -130,3 +130,34 @@ let oStr = "AaBbCc";
 let moStr = oStr.replace(/([A-Z])([a-z])/g, '$2$1' )
 console.log(moStr) // 'aAbBcC'
 
+/*
+Pig Latin is a way of altering English Words. The rules are as follows:
+
+- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+
+- If a word begins with a vowel, just add way at the end.
+*/
+
+function translatePigLatin(str) {
+
+
+let checker = /^[aeiou]/;
+let vowelless = /[^aeiou]+/g;
+console.log(vowelless.test(str))
+
+
+
+if (checker.test(str) === true) {
+  return str.replace(/^([aeiou])([a-z]+)/g, '$1$2way');
+} else if (vowelless.test(str) === false) {
+  return str.replace(/([^aeiou]+)/g, '$1ay' );
+}
+return str.replace(/^([^aeiou]+)([a-z]+)/g, '$2$1ay');
+}
+
+
+
+
+
+console.log(translatePigLatin("rhythm"));
+
