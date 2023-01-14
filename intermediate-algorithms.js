27f17199acaf56abfd548cfa0a725cf697b87152
Charles <CharlesCrowleyDev@gmail.com>
2023-01-14 19:56:26 +0100

    Update intermediate-algorithms.js

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -191,4 +191,15 @@ console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "l
 console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting")) //He is Sitting on the couch
 console.log(myReplace("I think we should look up there", "up", "Down")) //I think we should look down there
 
+/*
+DNA Pairing
+Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
+
+The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+*/
+
 
