5c6fa301ca61fb9f4a0b41a206d9c5e625e67ed9
Charles <CharlesCrowleyDev@gmail.com>
2023-01-26 12:23:25 +0100

    Update intermediate-algorithms.js

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -240,4 +240,22 @@ function fearNotLetter(str) {
 }
 
 console.log(fearNotLetter("abce")); // returns d
+/*
+Sorted Union
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
 
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
+*/
+
+function uniteUnique(...arr) {
+  let spreadArr = [].concat(...arr)
+  return spreadArr.filter((item, index) => spreadArr.indexOf(item) === index);
+
+
+}
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])); // [ 1, 3, 2, 5, 4 ]
