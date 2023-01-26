5e49c4df54a0ff8bcc3d46d841b69f38f672bfe2
Charles <CharlesCrowleyDev@gmail.com>
2023-01-26 13:03:40 +0100

    Update intermediate-algorithms.js

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -259,3 +259,20 @@ function uniteUnique(...arr) {
 }
 
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])); // [ 1, 3, 2, 5, 4 ]
+
+/*
+Convert HTML Entities
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
+
+
+function convertHTML(str) {
+  return str.replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+}
+
+
+console.log(convertHTML("Schindler's List")); // Schindler&apos;s List
