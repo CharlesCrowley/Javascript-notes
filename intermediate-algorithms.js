55622ca939dbd710a1c258a8e2968a495ed9b232
Charles <CharlesCrowleyDev@gmail.com>
2023-02-11 14:38:58 +0100

    Update intermediate-algorithms.js

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -334,3 +334,30 @@ function sumPrimes(num) {
 
 console.log(sumPrimes(10))
 
+
+/*
+Smallest Common Multiple
+Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+
+The range will be an array of two numbers that will not necessarily be in numerical order.
+
+For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+*/
+
+function smallestCommons(arr) {
+  arr.sort((a, b) => a - b);
+  let [small, big] = arr;
+
+  let lcm = big;
+  for (let i = big - 1; i >= small; i--) {
+    if (lcm % i !== 0) {
+      lcm += big;
+      i = big;
+    }
+  }
+
+  return lcm;
+}
+smallestCommons([1,5]);
+
+
