cb8c1dc74ce7557bd9208c9c626d5d68ac5b1ada
Charles <CharlesCrowleyDev@gmail.com>
2023-02-14 12:40:02 +0100

    Update Cert-Projects.js

--- Cert-Projects.js
+++ Cert-Projects.js
@@ -17,3 +17,25 @@ function palindrome(str) {
   return str === reversed;
 }
 console.log(palindrome("almosttsomla.")) // true
+
+/*
+Roman Numeral Converter
+Convert the given number into a roman numeral.
+*/ 
+
+function convertToRoman(num) {
+    var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+    var romanized = '';
+
+    for (var index = 0; index < decimalValue.length; index++) {
+        while (decimalValue[index] <= num) {
+            romanized += romanNumeral[index];
+            num -= decimalValue[index];
+        }
+    }
+    return romanized;
+}
+
+console.log(convertToRoman(83)); //LXXXIII
+console.log(convertToRoman(1023)); //MXXIII
