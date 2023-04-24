46f0b5eb4ae674d08f4581c9e4cb00f677ec5ed5
Charles <CharlesCrowleyDev@gmail.com>
2023-04-24 23:54:22 +0200

    Update Cert-Projects.js

--- Cert-Projects.js
+++ Cert-Projects.js
@@ -71,4 +71,39 @@ function rot13(input) {
 }
 
 console.log(rot13("HELLO"));
+/*
+Telephone Number Validator
+Return true if the passed string looks like a valid US phone number.
 
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. 
+The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. 
+Your job is to validate or reject the US phone number based on any combination of the formats provided above. 
+The area code is required. If the country code is provided, you must confirm that the country code is 1. 
+Return true if the string is a valid US phone number; otherwise return false.
+
+*/
+
+function telephoneCheck(str) {
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
+  /*
+  ^ matches the start of the string
+  (1\s?)? matches an optional "1" at the beginning, followed by an optional space
+  (\(\d{3}\)|\d{3}) matches either a three-digit number enclosed in parentheses, or a plain three-digit number
+  [\s-]? matches an optional space or hyphen
+  \d{3} matches three digits
+  [\s-]? matches an optional space or hyphen
+  \d{4} matches four digits
+  $ matches the end of the string
+  */
+  return regex.test(str);
+}
+
+telephoneCheck("555-555-5555");
